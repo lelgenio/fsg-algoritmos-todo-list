5da0ae872c9ed6514bac99d530cca7218dd6e53c
lelgenio <lelgenio@disroot.org>
2021-05-16 14:40:48 -0300

    todo: add unit tests

---
+++ tests/todo.c
@@ -0,0 +1,25 @@
+#include "test_util.h"
+#include <stdlib.h>
+
+#include "todo.h"
+
+int main() {
+    struct Todo_list todo_list = todo_list_create();
+
+    assert_eq(todo_list.size, 0);
+
+    todo_list_add(&todo_list, "my title 1", "my text 1");
+    todo_list_add(&todo_list, "my title 2", "my text 2");
+
+    assert_eq(todo_list.size, 2);
+
+    assert_eq(todo_list.first[1].title[0], 'm');
+    assert_eq(todo_list.first[1].title[1], 'y');
+
+    todo_list_remove(&todo_list, 0);
+    todo_list_remove(&todo_list, 1);
+
+    assert_eq(todo_list.size, 0);
+
+    return 0;
+}
